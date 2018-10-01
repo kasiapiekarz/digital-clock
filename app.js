257893cb17bcf665376fbc6a5685e652d11063f6
Kasia Piekarz <katjerinka@gmail.com>
2018-10-01 16:50:12 +0200

    updating hour,,min,sec, changing body bckground color

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const body = document.querySelector("body");
-const h1 = document.querySelector("h1");
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.querySelector("body");
+    const h1 = document.querySelector("h1");
 
-function whatTime() {
-    const d = new Date();
-    const hour = d.getHours();
-    const min = d.getMinutes();
-    const sec = d.getSeconds();
-    const time = hour + ":" + min + ":" + sec;
-    console.log(time);
-    return time;
-}
+    function showHexColor() {
+        const d = new Date();
+        let hour = d.getHours();
+        let min = d.getMinutes();
+        let sec = d.getSeconds();
+        
+        if (hour.toString().length < 2) {
+            hour = "0" + hour;
+        }
+        if (min.toString().length < 2) {
+            min = "0" + min;
+        }
+        if (sec.toString().length < 2) {
+            sec = "0" + sec;
+        }
+        const time = hour + ":" + min + ":" + sec;
+        const hexColor = "#" + hour + min + sec;
+        body.style.backgroundColor = hexColor;
+        return time;
+    }
 
-function showTime() {
-    h1.textContent = whatTime();
-}
-setInterval(showTime, 1000);
+    function showTime() {
+        h1.textContent = showHexColor();
+    }
+    setInterval(showTime, 1000);
+});
